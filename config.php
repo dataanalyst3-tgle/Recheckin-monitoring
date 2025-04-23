aeedf9b2f69a4680828ba79d71808ed5429b48d1
dataanalyst3-tgle <data_analyst3@grandlounge.com.mx>
2025-04-23 11:01:42 -0600

    config.php

--- config.php
+++ config.php
@@ -1,19 +1,1 @@
-<?php
-$host = 'tableau.tgle.mx';
-$port = 3306;
-$dbname = 'tgle';
-$username = 'fivetran';
-$password = '_$RF5x]9eac-Gz4,';
 
-try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    // Establecer el juego de caracteres después de conectar (opcional pero recomendado)
-    $conn->exec("SET NAMES 'utf8mb4'");
-    $conn->exec("SET CHARACTER SET utf8mb4");
-} catch(PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
-}
-?>
